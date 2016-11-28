b404bedf3a6fd92c7d7c737728dc9217e542003a
Deac <deac.karns@gmail.com>
2016-11-27 22:02:41 -0500

    add docker container php configuration

--- www/index.php
+++ www/index.php
@@ -1,3 +1,19 @@
 <?php
-phpinfo();
-?>
+// Version
+define('VERSION', '2.3.0.3_rc');
+
+// Configuration
+if (is_file('config.php')) {
+	require_once('config.php');
+}
+
+// Install
+if (!defined('DIR_APPLICATION')) {
+	header('Location: install/index.php');
+	exit;
+}
+
+// Startup
+require_once(DIR_SYSTEM . 'startup.php');
+
+start('catalog');
